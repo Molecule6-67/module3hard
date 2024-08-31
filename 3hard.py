952b4dd3e04a935f9b0bcef2c2aa368ef8a3ed5e
Aydar Sadykov <6darch6@vk.com>
2024-08-31 18:34:09 +0500

    Add files via upload

---
+++ 3hard.py
@@ -0,0 +1,31 @@
+data_str = [
+    [1, 2, 3],
+    {'a': 4, 'b': 5},
+    (6, {'cube': 7, 'drum': 8}),
+    "Hello",
+    ((), [{(2, 'Urban', ('Urban2', 35))}])
+]
+
+
+def calculate_structure_sum(data_structure):
+    total = 0
+    if isinstance(data_structure, str):
+        total += len(data_structure)
+
+    elif isinstance(data_structure, (int, float, complex)):
+        total += data_structure
+
+    elif isinstance(data_structure, (list, set, tuple)):
+        for item in data_structure:
+            total += calculate_structure_sum(item)
+
+    elif isinstance(data_structure, dict):
+        for key, value in data_structure.items():
+            total += calculate_structure_sum(key)
+            total += calculate_structure_sum(value)
+
+    return total
+
+
+result = calculate_structure_sum(data_str)
+print(result)
